49b80676f62db46cc76cf8e0437f3222fc30deb1
DiljahanToyshi <diljahantoyshi@gmail.com>
2023-12-28 11:44:55 +0600

    responsive the updateModal

--- src/Components/AddToys.jsx
+++ src/Components/AddToys.jsx
@@ -37,18 +37,18 @@ const AddToys = () => {
   //   console.log(user);
   return (
     <div className="">
-      <h1 className="sm:text-2xl md:text-4xl font-bold text-indigo-600 text-center">Add your Toy's Collection</h1>
+      <h1 className="title-text pb-2 ">Add your Toy's Collection</h1>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="text-center md:flex gap-14 justify-center mt-4 md:mt-8">
+        <div className="text-center md:flex gap-10 lg:gap-14 justify-center">
           {errors.exampleRequired && <span>This field is required</span>}
-          <div>
+          <div className="w-full px-2">
             {" "}
             <div>
               {" "}
               <label  htmlFor="Toy Name" className="text-xl font-normal mb-1">Toy Name</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full p-2 rounded-md outline-indigo-600"
                 {...register("Toy Name")}
                 placeholder="Toy Name"
               />
@@ -57,7 +57,7 @@ const AddToys = () => {
               {" "}
               <label className="text-xl font-normal mb-1" htmlFor="Seller Name">Seller Name</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full p-2 rounded-md outline-indigo-600"
                 {...register("sellerName", { required: true })}
                 placeholder="Seller Name"
               />
@@ -65,7 +65,7 @@ const AddToys = () => {
             <div>
               <label className="text-xl font-normal mb-1" htmlFor="Quantity">Quantity</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full p-2 rounded-md outline-indigo-600"
                 {...register("Available Quantity", { required: true })}
                 placeholder="AvailableQuantity"
                 type="number"
@@ -74,7 +74,7 @@ const AddToys = () => {
             <div className="my-4">
               {" "}
               <label className="text-xl font-normal mb-1" htmlFor="Category">Category</label> <br />
-              <select className="text-input p-2 rounded-sm" {...register("subCategory")}>
+              <select className="text-input w-full p-2 rounded-sm" {...register("subCategory")}>
                 <option value="Disney Princess">Disney Princess</option>
                 <option value="Frozen Dolls">Frozen Dolls</option>
                 <option value="Animation Characters">
@@ -83,13 +83,13 @@ const AddToys = () => {
               </select>
             </div>{" "}
           </div>
-          <div>
+          <div className="w-full px-2">
             {" "}
             <div>
               {" "}
               <label className="text-xl font-normal mb-1" htmlFor="Picture">Picture</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full p-2 rounded-md outline-indigo-600"
                 {...register("picture")}
                 placeholder="picture"
                 type="url"
@@ -99,7 +99,7 @@ const AddToys = () => {
               {" "}
               <label className="text-xl font-normal mb-1" htmlFor="Price">Price</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full p-2 rounded-md outline-indigo-600"
                 {...register("price")}
                 placeholder="price"
                 type="number"
@@ -109,27 +109,30 @@ const AddToys = () => {
               {" "}
               <label className="text-xl font-normal mb-1" htmlFor="Email">Email</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full p-2 rounded-md outline-indigo-600"
                 defaultValue={user?.email}
                 {...register("sellerEmail")}
                 type="email" readOnly
               />
             </div>
-            <div className="my-4">
+            <div className="my-4 ">
               <label className="text-xl font-normal mb-1" htmlFor="Description">Description</label> <br />
               <input
-                className="text-input p-2 rounded-sm"
+                className="text-input w-full h-20 md:h-auto outline-indigo-600 p-2 rounded-sm"
                 {...register("description")}
                 placeholder="description"
               />
             </div>
           </div>
         </div>
+        <div className="text-center">
+
         <input
-          className=" btn submit-btn rounded-md p-3 border-2 border-indigo-500  bg-indigo-500 text-white font-semibold md:ml-96"
+          className=" btn submit-btn rounded-md p-3 border-2 border-indigo-500  bg-indigo-500 text-white font-semibold "
           value="Add a toy"
           type="submit"
-        />
+        />        </div>
+
       </form>
     </div>
   );
